c44b141dd3e78ca3436af41577d611d370c97a37
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2022-11-15 09:54:50 +0200

    init project

---
+++ src/main/java/com/example/abstractroutingtask/AbstractRoutingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.abstractroutingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractRoutingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbstractRoutingTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/abstractroutingtask/AbstractRoutingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abstractroutingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractRoutingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
