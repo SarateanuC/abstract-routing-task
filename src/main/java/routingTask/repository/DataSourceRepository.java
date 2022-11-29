00841fa46cda4f74f432a92b6f7539d9d16ac7da
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2022-11-29 10:30:55 +0200

    A001: fix bug

--- src/main/java/routingTask/repository/DataSourceRepository.java
+++ src/main/java/routingTask/repository/DataSourceRepository.java
@@ -1,17 +1,21 @@
 package routingTask.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import routingTask.entity.DbConnection;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-public interface DataSourceRepository extends JpaRepository<DbConnection,String> {
-    @Query(value="select * from decrypted()", nativeQuery=true)
+public interface DataSourceRepository extends JpaRepository<DbConnection, String> {
+    @Query(value = "select * from decrypted()", nativeQuery = true)
     List<DbConnection> getConnections();
 
-    @Query(value = " call insert_data(?1,?2,?3,?4)",nativeQuery = true)
-    void addConnection(String id,String url, String username, String password);
+    @Transactional
+    @Modifying
+    @Query(value = " call insert_data(?1,?2,?3,?4)", nativeQuery = true)
+    void addConnection(String id, String url, String username, String password);
 }
